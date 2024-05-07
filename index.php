eb46ac99e312a1aa93edcb7afbd2c3703c811bd0
Tommy Hendrawan <tommy.avicenna@gmail.com>
2024-05-07 15:35:25 +0700

    logo, user ilang, background dll

--- index.php
+++ index.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
-    <title>Helpdesk BPS - YPAP</title>
+    <title>Timeline Project</title>
         <!--<link rel="icon" href="/images/favicon/favicon-32x32.png" sizes="32x32">-->
         <link rel="icon" href="img/favicon-32x32.ico" type="image/x-icon" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"/>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap"/>
     <link rel="stylesheet" href="css/mdb.min.css" />
 </head>
-<body style="background-image: url('image/rm380-14.jpg'); background-size: 1430px 1100px;">
+<body style="background-image: url('image/background_custom.png'); background-size: 1800px 1200px;">
 <!-- your project is here -->
 <section class="vh-100">
     <div class="container py-5 h-100">
@@ -22,29 +22,29 @@
                     <div class="mb-3 mt-3">
                         <div class="container text-center">
                             <p></p>
-                            <h4>TIMELINE PROJECT</h4>
+                            
                         </div>
                     </div>
                     <p align="center">
-                        <img src="image/avicenna-helpdesk.png" style="max-width: 90%">
+                        <img src="image/timepro.png" style="max-width: 90%">
                     </p>
                     <div class="card-body text-center">
-                        <h5 class="mb-4">Sign in</h5>
-                        <button onclick="location.href='dashboard.php'" class="btn btn-lg btn-block btn-primary" style="background-color: #15bee8;"
-                                type="submit"><i></i>USER</button>
-                        <button onclick="location.href='login.html'" class="btn btn-lg btn-block btn-primary mb-2" style="background-color: #15bee8;"
+                        <h6 class="mb-3">LOGIN</h6>
+                        <button onclick="location.href='login.html'" class="btn btn-lg btn-block btn-primary mb-5" style="background-color: #15bee8;"
                                 type="submit"><i></i>EMPLOYEE</button>
                                 <p></p>
-                        
+
+                                
+                                <div class="container text-center">
+                        Copyright &copy; 2024 <a class="white-text"></a>
+                    </div>
+                    
+                        <span class="right">Powered By <a class>Tommy .H</a></span>
 		            	       
                     </div>
                 </div>
                 <div class="footer-copyright mt-3">
-                    <div class="container text-center">
-                        Copyright &copy; 2022 <a class="grey-text">Transformasi Digital - BPS YPAP</a> All rights reserved.
-                    </div>
-                    <div class="container text-center">
-                        <span class="right"> Powered By  <a class>Tommy .H</a></span>
+                    
                     </div>
                 </div>
             </div>
