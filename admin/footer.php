f45d8e0c3c682641cd4864db6a92e05d6ed9a3e4
adi alam <adikuasa.mangkualam@ist.id>
2024-05-21 18:48:16 +0700

    fixing footer

--- admin/footer.php
+++ admin/footer.php
@@ -1,8 +1,10 @@
 <footer class="page-footer">
-        <div class="footer-copyright">
+    <div class="footer-copyright">
+        <nav class="cyan">
             <div class="container">
                 Copyright &copy; 2024 <a class="grey-text text-lighten-4" href="#" target="_blank">www.altech.web.id</a> All rights reserved.
-                <span class="right"> Powered By  <a class="grey-text text-lighten-4" href="#">Altech Indonesia</a></span>
+                <span class="right"> Powered By <a class="grey-text text-lighten-4" href="#">Altech Indonesia</a></span>
             </div>
-        </div>
-    </footer>
+        </nav>
+    </div>
+</footer>
--- admin/header.php
+++ admin/header.php
@@ -1,21 +1,9 @@
 <header id="header" class="page-topbar">
-        <!-- start header nav-->
-        <div class="navbar-fixed">
-            <nav class="cyan">
-                <div class="nav-wrapper">
-                    <h5 class="logo-wrapper"><a href="index.php" class="brand-logo darken-1"><!-- <img src="images/materialize-logo.png" alt="materialize logo">--> <span class="logo-text">Timeline Project | My Jobs<i class="mdi-action-delete"></i> </span>   </a> </h5>
-                    <!-- <ul class="right hide-on-med-and-down">
-                        <li class="search-out">
-                            <input type="text" class="search-out-text">
-                        </li>
-                        <li>    
-                            <a href="javascript:void(0);" class="waves-effect waves-block waves-light show-search"><i class="mdi-action-search"></i></a>                              
-                        </li>
-                        <li><a href="javascript:void(0);" class="waves-effect waves-block waves-light toggle-fullscreen"><i class="mdi-action-settings-overscan"></i></a>
-                        </li>
-                    </ul> -->
-                </div>
-            </nav>
-        </div>
-        <!-- end header nav-->
-    </header>
+    <div class="navbar-fixed">
+        <nav class="cyan">
+            <div class="nav-wrapper">
+                <h5 class="logo-wrapper"><a href="index.php" class="brand-logo darken-1"><!-- <img src="images/materialize-logo.png" alt="materialize logo">--> <span class="logo-text">Timeline Project | My Jobs</span> </a> </h5>
+            </div>
+        </nav>
+    </div>
+</header>
