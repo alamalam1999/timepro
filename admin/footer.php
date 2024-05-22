72f83702c37c279a1b3b34137c1c93485c790915
Tommy Hendrawan <tommy.avicenna@gmail.com>
2024-05-22 09:48:31 +0700

    css:906 : header : footer ya di

--- admin/footer.php
+++ admin/footer.php
@@ -1,9 +1,9 @@
 <footer class="page-footer">
     <div class="footer-copyright">
         <nav class="cyan">
-            <div class="container">
-                Copyright &copy; 2024 <a class="grey-text text-lighten-4" href="#" target="_blank">www.altech.web.id</a> All rights reserved.
-                <span class="right"> Powered By <a class="grey-text text-lighten-4" href="#">Altech Indonesia</a></span>
+            <div class="container" >
+                <a class="black-text text-lighten-4" target="_blank">Copyright &copy; 2024  www.altech.web.id All rights reserved.</a>
+                <span class="right"> Powered By <a class="black-text text-lighten-4" href="#">Altech Indonesia</a></span>
             </div>
         </nav>
     </div>
--- admin/header.php
+++ admin/header.php
@@ -2,7 +2,7 @@
     <div class="navbar-fixed">
         <nav class="cyan">
             <div class="nav-wrapper">
-                <h5 class="logo-wrapper"><a href="index.php" class="brand-logo darken-1"><!-- <img src="images/materialize-logo.png" alt="materialize logo">--> <span class="logo-text">Timeline Project | My Jobs</span> </a> </h5>
+                <h4 class="logo-wrapper"><a href="index.php" class="brand-logo darken-1"><!-- <img src="images/materialize-logo.png" alt="materialize logo">--> <span class="logo-text black-text text-lighten-4">Timeline Project | My Jobs</span></a> </h4>
             </div>
         </nav>
     </div>
